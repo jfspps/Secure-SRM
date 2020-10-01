ea17aa2f5b1c3c163c83c5511b953b7c63ccfdf4
jfspps <jfsapps@gmail.com>
2020-10-01 23:54:30 +0100

    impl new AssignmentType with tests

--- src/main/java/com/secure_srm/web/controllers/AssignmentTypeController.java
+++ src/main/java/com/secure_srm/web/controllers/AssignmentTypeController.java
@@ -2,15 +2,14 @@ package com.secure_srm.web.controllers;
 
 import com.secure_srm.model.academic.AssignmentType;
 import com.secure_srm.services.academicServices.AssignmentTypeService;
+import com.secure_srm.web.permissionAnnot.AdminCreate;
 import com.secure_srm.web.permissionAnnot.TeacherRead;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Set;
 
@@ -36,5 +35,28 @@ public class AssignmentTypeController {
         Set<AssignmentType> assignmentTypeSet = assignmentTypeService.findAll();
         model.addAttribute("assignmentTypeSet", assignmentTypeSet);
         return "/SRM/assignmentType/assignmentTypeIndex";
+    }
+
+    @AdminCreate
+    @GetMapping("/new")
+    public String getNewAssignmentType(Model model){
+        model.addAttribute("assignmentType", AssignmentType.builder().build());
+        return "/SRM/assignmentType/newAssignmentType";
+    }
+
+    @AdminCreate
+    @PostMapping("/new")
+    public String postNewAssignmentType(@ModelAttribute("assignmentType") AssignmentType assignmentType, Model model){
+        if (assignmentTypeService.findByDescription(assignmentType.getDescription()) != null){
+            log.debug("Assignment type with given description already exists");
+            model.addAttribute("assignmentType", assignmentType);
+            model.addAttribute("assignmentTypeFeedback", "Assignment type with given description already exists");
+            return "/SRM/assignmentType/newAssignmentType";
+        }
+
+        AssignmentType saved = assignmentTypeService.save(assignmentType);
+        log.debug("New assignment type with ID: " + saved.getId() + " saved");
+        model.addAttribute("assignmentTypeFeedback", "New assignment type saved");
+        return "/SRM/assignmentType/newAssignmentType";
     }
 }
--- src/test/java/com/secure_srm/web/controllers/AssignmentTypeController_IT.java
+++ src/test/java/com/secure_srm/web/controllers/AssignmentTypeController_IT.java
@@ -7,8 +7,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.transaction.Transactional;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @Slf4j
@@ -23,5 +25,25 @@ public class AssignmentTypeController_IT extends SecurityCredentialsTest{
                 .andExpect(view().name("/SRM/assignmentType/assignmentTypeIndex"))
                 .andExpect(status().isOk())
                 .andExpect(model().attributeExists("assignmentTypeSet"));
+    }
+
+    @MethodSource("com.secure_srm.web.controllers.SecurityCredentialsTest#streamSchoolAdminUsers")
+    @ParameterizedTest
+    void getNewAssignment(String username, String pwd) throws Exception {
+        mockMvc.perform(get("/assignmentTypes/new").with(httpBasic(username, pwd)))
+                .andExpect(view().name("/SRM/assignmentType/newAssignmentType"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("assignmentType"));
+    }
+
+    @MethodSource("com.secure_srm.web.controllers.SecurityCredentialsTest#streamSchoolAdminUsers")
+    @ParameterizedTest
+    void postNewAssignment(String username, String pwd) throws Exception {
+        mockMvc.perform(post("/assignmentTypes/new").with(httpBasic(username, pwd)).with(csrf())
+                .param("description", "sample assignment description"))
+                .andExpect(view().name("/SRM/assignmentType/newAssignmentType"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("assignmentType"))
+                .andExpect(model().attributeExists("assignmentTypeFeedback"));
     }
 }
